39564dff7ab186bdb248b03d929ec24ffb025d3c
Matteus Moreno <matteus_moreno@live.com>
2023-10-20 16:05:24 -0300

    adicionando PaymentDocumentController

---
+++ src/main/java/mgmgroup/paymentsystem/payment_document/controller/PaymentDocumentController.java
@@ -0,0 +1,9 @@
+package mgmgroup.paymentsystem.payment_document.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment_document")
+public class PaymentDocumentController {
+}
