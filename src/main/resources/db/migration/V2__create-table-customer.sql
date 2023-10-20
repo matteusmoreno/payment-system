4f803ec961e4e8672c36504ea59377bb9ed6c431
Matteus Moreno <matteus_moreno@live.com>
2023-10-20 19:13:58 -0300

    criando a migração costumer

---
+++ src/main/resources/db/migration/V2__create-table-customer.sql
@@ -0,0 +1,19 @@
+CREATE TABLE customer (
+    id BINARY(16) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(20),
+    birth_date DATE,
+    gender VARCHAR(10),
+    cpf VARCHAR(15) UNIQUE,
+    cep VARCHAR(10),
+    country VARCHAR(50),
+    state VARCHAR(50),
+    city VARCHAR(50),
+    neighborhood VARCHAR(50),
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
+    active TINYINT(1),
+    PRIMARY KEY (id)
+);
